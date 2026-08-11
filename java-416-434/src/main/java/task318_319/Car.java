9e8594e23952e46ff8d3aaa3634faeac7d2e4b6a
unknown <reemalharrasi00@gmail.com>
2026-08-11 16:14:15 +0400

    created car class

---
+++ java-416-434/src/main/java/task318_319/Car.java
@@ -0,0 +1,15 @@
+package task318_319;
+
+public class Car {
+    String colour;
+    String plate;
+
+    //method
+    public void drive() {
+        System.out.println(this.colour + " " + this.plate + " car has moved!");
+    }
+
+    public void brake() {
+        System.out.println(this.colour + " " + this.plate + " car has stopped!");
+    }
+}
---
+++ java-416-434/src/main/java/task318_319/Main.java
@@ -0,0 +1,4 @@
+package task318_319;
+
+public class Main {
+}
