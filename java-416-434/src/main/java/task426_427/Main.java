e1776b5ce1fb32c7cb123ac2ccf5fc581e5f2708
ReemAlharrasi <re2ali5em@gmail.com>
2026-08-11 21:04:15 +0400

    updated main class - task427

--- java-416-434/src/main/java/task426_427/Main.java
+++ java-416-434/src/main/java/task426_427/Main.java
@@ -5,5 +5,11 @@ public class Main {
     void main(){
         CashPayment cash = new CashPayment();
         cash.process();
+
+        CardPayment card = new CardPayment();
+        card.process();
+
+        PayPalPayment paypal = new PayPalPayment();
+        paypal.process();
     }
 }
