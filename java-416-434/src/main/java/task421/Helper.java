452271b347075d5d0c9efa8f3523f4f2be56ee22
unknown <reemalharrasi00@gmail.com>
2026-08-11 16:25:23 +0400

    solved 421

---
+++ java-416-434/src/main/java/task421/Helper.java
@@ -0,0 +1,10 @@
+package task421;
+
+public class Helper {
+    void main(){
+        Helper.hello();
+    }
+    static void hello(){ //without static it can not be called directly
+        System.out.println("Hello from Codeline");
+    }
+}
