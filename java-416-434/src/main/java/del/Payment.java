ee5aff9033c98563d50b60310ae300d4b8817267
unknown <reemalharrasi00@gmail.com>
2026-08-12 08:36:29 +0400

    created payment class

---
+++ java-416-434/src/main/java/del/Payment.java
@@ -0,0 +1,8 @@
+package task426_427_428;
+
+public class Payment {
+
+    public void process(){
+        System.out.println("processing. . .");
+    }
+}
--- java-416-434/src/main/java/task426_427_428_429/Payment.java
+++ java-416-434/src/main/java/task426_427_428_429/Payment.java
@@ -1,8 +1,4 @@
-package task426_427_428;
+package task426_427_428_429;
 
 public class Payment {
-
-    public void process(){
-        System.out.println("processing. . .");
-    }
 }
